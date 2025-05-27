5b5579a5ad87e47ebe65ff2af619e86146130757
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-05-26 20:44:27 -0500

    completed throught Rendering Data In Templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("cats/", views.cat_index, name="cat-index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,10 +3,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Cat class
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Cat instances
+cats = [
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
 # home view function
 def home(req):
-    return HttpResponse('<h1>hi</h1>')
+    # return HttpResponse('<h1>hi</h1>')
+    return render(req, 'home.html')
 
+# about view
 def about(req):
     # return HttpResponse('<h1>about</h1>')
     return render(req, 'about.html')
+
+# all cats view
+def cat_index(req):
+    return render(req, 'cats/index.html', {'cats': cats})
+
